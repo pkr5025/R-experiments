c7d62737cf954809ea2afffd394897ed1df6dba7
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-05-03 20:30:05 -0500

    Add files via upload

--- sample.r
+++ sample.r
@@ -1,7 +1,12 @@
 # Tutorial sample session
+library("tcltk")
 norms <- function(){
 	x <- rnorm(50)
 	y <- rnorm(x)
+	quartz()
 	plot(x,y)
+	prompt <- "hit spacebar"
+	extra <- "something extra"
+	capture <- tk_messageBox(message = prompt, detail = extra)
 }
 norms()
