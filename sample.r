b7fbe3b27716bb9f2e967e408053c2c24af5d6ff
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-05-03 20:38:00 -0500

    Playing with plotting

--- sample.r
+++ sample.r
@@ -8,5 +8,20 @@ norms <- function(){
 	prompt <- "hit spacebar"
 	extra <- "something extra"
 	capture <- tk_messageBox(message = prompt, detail = extra)
+	rm(x,y)
+
+	x <- 1:20
+	w <- 1+sqrt(x)/2
+	dummy <- data.frame(x=x, y=x+rnorm(x)*w)
+	fm <- lm(y~x, data = dummy)
+	summary(fm)
+	fm1 <- lm(y~x, data = dummy, weight = 1/w^2)
+	summary(fm1)
+	attach(dummy)
+	lrf <- lowess(x,y)
+	plot(x,y)
+	lines(x,lrf$y)
+	capture <- tk_messageBox(message = prompt, detail = extra)
+
 }
 norms()
