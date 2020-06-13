f11f8a2d1d16856bf6b967c79d8ed00742384005
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-06-12 22:02:57 -0500

    finished 4.2

--- Ch4.r
+++ Ch4.r
@@ -12,6 +12,18 @@ Ch4.2 <- function(){
 	incomes <- c(65, 40, 32, 56, 120, 400, 500)
 	incmeans <- tapply(incomes, somestatesf, mean)
 	print(incmeans)
+
+	stdError <- function(x) sqrt(var(x)/length(x))
+	incster <- tapply(incomes, somestatesf, stdError)
+	print(incster)
+
+	samplesizes <- tapply(incomes, somestatesf, length)
+	print(samplesizes)
+	qt(.95, samplesizes)
+
 	}
+Ch4.3 <- function(){
+	}
+
 
 Ch4.2()
