e8a7b778200a8c5c751e4be17efdc1a66f7b164c
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-06-14 22:29:30 -0500

    5.2 done

--- Ch5.r
+++ Ch5.r
@@ -4,8 +4,25 @@
 
 Ch5.1 <- function(){
 	z <- 1:125
+	#treat z as a 5 x 5 x 5 array
 	dim(z) <- c(5,5,5)
 	print(z)
+	cat("\n")
 	}
 
+#5.2 Array indexing, subsections of an array
+
+Ch5.2 <- function(){
+	z <- 1:125
+	#treat z as a 5 x 5 x 5 array
+	dim(z) <- c(5,5,5)
+	
+	# print subsection of the array
+	print(z[2,,])
+		
+	}
+
+
+
 Ch5.1()
+Ch5.2()
