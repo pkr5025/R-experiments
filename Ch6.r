065c346edf113cedec5606e2e4cef8a524ddd489
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-07-12 22:11:52 -0500

    Finished 6.1

---
+++ Ch6.r
@@ -0,0 +1,25 @@
+# 6.1 Lists
+Ch6.1 <- function(){
+	Lst <- list(name = "Philip", wife = "Sarah", no.children=2, child.ages=c(5,7,12))
+	print(Lst)
+	cat("\n")
+	print(Lst$name)
+	cat("\n")
+	print(Lst[[2]])
+	cat("\n")
+	print(Lst[2]) #for comparison 
+	}
+
+
+Ch6.2 <- function(){
+	}
+Ch6.3 <- function(){
+	}
+Ch6.4 <- function(){
+	}
+
+
+
+Ch6.1()
+Ch6.2()
+Ch6.3()
