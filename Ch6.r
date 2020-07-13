156a0f9e15cf22cda40fc97393ffef4cc65205fb
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-07-12 22:20:13 -0500

    Finished 6.2.1

--- Ch6.r
+++ Ch6.r
@@ -18,13 +18,23 @@ Ch6.2 <- function(){
 	Lst <- list(name_1 =a, name_2 = b, name_3 = c)
 	print(Lst)
 	}
-Ch6.3 <- function(){
+
+# 6.2.1 Concatenating lists
+Ch6.2.1 <- function(){
+	a = 1:3
+	b = 1:3
+	c = matrix(c(a,b), nrow=3, ncol=3)
+	Lst <- list(name_1 =a, name_2 = b, name_3 = c)
+	list.three <- c(Lst, Lst, Lst)
+	print(list.three)
+	
 	}
-Ch6.4 <- function(){
+Ch6.3 <- function(){
 	}
 
 
 
 #Ch6.1()
-Ch6.2()
-Ch6.3()
+#Ch6.2()
+Ch6.2.1()
+#Ch6.3()
