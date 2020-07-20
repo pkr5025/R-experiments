3da8b9cd573b59868800137462ea407be16964a5
pkr5025 <44106322+pkr5025@users.noreply.github.com>
2020-07-19 21:51:42 -0500

    Finished 9.2.1

--- Ch9.r
+++ Ch9.r
@@ -10,4 +10,23 @@ Ch9.1 <- function(){
 	
 	}
 
-Ch9.1()
+# 9.2 Control Statements
+# 9.2.1 Conditional execution: if statements
+Ch9.2.1 <- function(){
+	x<- 4
+	y<- 3*x
+	if (x >y) print("X is bigger") else print("Y is bigger")
+
+
+	# Easier to read on separate lines
+	if (x>y)
+		print("a")
+	else
+		print("b")
+
+		
+	}
+
+
+#Ch9.1()
+Ch9.2.1()
